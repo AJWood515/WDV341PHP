e15543c5584598a834958ffe58c59a6514f7286d
AJWood515 <32825644+AJWood515@users.noreply.github.com>
2018-01-29 20:04:29 -0600

    PHP Basics- Alex Wood

---
+++ PHPBasics.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html>
+  <body>
+    <h1>PHP Basics</h1>
+    <?php
+    $yourName = "Alex Wood";?>
+  <h2> <?php echo $yourName ?></h2>
+     <?php $number1 = 10;
+     $number2 = 15;
+     $total = $number1 + $number2;
+     echo $total;?>
+    </body>
+
+  <script>
+  //document.write('<h1>hello</h1>');
+
+
+  <?php $arr = array('PHP','HTML','Javascript');
+      echo "document.write('<p>$arr[0]<br/>$arr[1]<br/>$arr[2]<br/></p>')";
+  ?>
+
+  </script>
+<br/>
+  <a href= "https://github.com/AJWood515/WDV341PHP">Git Hub Repo</a>
+
+
+</html>
