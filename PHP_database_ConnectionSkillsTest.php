1d2018f9066d96835bb5903323a5f998db3f079a
AJWood515 <32825644+AJWood515@users.noreply.github.com>
2018-04-16 20:52:25 -0500

    Skils Test

--- PHP_database_ConnectionSkillsTest.php
+++ PHP_database_ConnectionSkillsTest.php
@@ -5,7 +5,7 @@ try {
    $conn = new PDO("mysql:host=$servername;dbname=ajwood515_wdv341", $username, $password);
    // set the PDO error mode to exception
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
+   echo "<script>console.log('Connected successfully') </script>";
    }
 catch(PDOException $e)
    {
